0dcf827eb6ca902139c9384f54b139ee656246f3
Stefan Buck <stefanb@brandwatch.com>
2017-07-09 17:12:06 +0200

    Listen to pjax:end event to invoke callback

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gitHubInjection = function (cb) {
+const gitHubInjection = cb => {
   if (!cb) {
     throw new Error('Missing argument callback');
   }
@@ -9,36 +9,12 @@ var gitHubInjection = function (cb) {
     throw new Error('Callback is not a function');
   }
 
-  var domElement = document.querySelector('#js-repo-pjax-container, #js-pjax-container');
-  if (!domElement || typeof MutationObserver === 'undefined') {
-    return cb();
-  }
-
-  var viewSpy = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      if (mutation.addedNodes.length) {
-        cb();
-      }
-    });
-  });
-
-  viewSpy.observe(domElement, {
-    childList: true
-  });
-
+  document.addEventListener('pjax:end', cb);
   cb();
-};
-
-// Export the gitHubInjection function for **Node.js**, with
-// backwards-compatibility for the old `require()` API. If we're in
-// the browser, add `gitHubInjection` as a global object.
-if (typeof exports !== 'undefined') {
-  if (typeof module !== 'undefined' && module.exports) {
-    exports = module.exports = gitHubInjection;
-  }
-  exports.gitHubInjection = gitHubInjection;
-} else {
-  /*jshint -W040 */
-  this.gitHubInjection = gitHubInjection;
-  /*jshint +W040 */
 }
+
+// Export the gitHubInjection function for **Node.js**
+// Otherwise leave it as a global
+ if (typeof exports !== 'undefined') {
+   exports.gitHubInjection = gitHubInjection;
+ }
